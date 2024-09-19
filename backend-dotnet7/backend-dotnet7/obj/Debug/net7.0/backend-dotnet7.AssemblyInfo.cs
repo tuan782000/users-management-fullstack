6c1746228414630004218cdfd8a4f2ea2e7cba3c
tuan782000 <thaituan7820@gmail.com>
2024-09-19 12:47:49 +0700

    thêm authentication ()

--- backend-dotnet7/backend-dotnet7/obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
+++ backend-dotnet7/backend-dotnet7/obj/Debug/net7.0/backend-dotnet7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b881be6e7eaedecf6ef79fd60c8256f6940d943d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf0ca96989b29c59c1d9081e467be41ead1e38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
